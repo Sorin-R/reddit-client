21aead7a69bed4f00f49a3a769a806a9b7b1dae0
SorinRotaru <info@rotarusorin.com>
2026-01-14 09:29:51 +0000

    chore: scaffold react app with vite

---
+++ reddit-client/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
+      <h1>Reddit Client</h1>
+      <p>App is running locally.</p>
+    </div>
+  );
+}
